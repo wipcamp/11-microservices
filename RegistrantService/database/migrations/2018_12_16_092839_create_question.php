b4a08a5549b4e1eee11ed17e3399dd488a2c66df
JaysZila <methaporn.jz@mail.kmutt.ac.th>
2018-12-16 18:55:27 +0700

    [ADD] migrate Questions and seed Questions class

---
+++ RegistrantService/database/migrations/2018_12_16_092839_create_question.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestion extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('content');
+            $table->integer('intelligent_weight');
+            $table->integer('communication_weight');
+            $table->integer('creative_weight');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
