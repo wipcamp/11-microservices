9457482a8c7d13db5ef967aefa90450ae2dc0d3d
Mild-TN <mild20091931@gmail.com>
2018-12-16 18:04:18 +0700

    [ADD] create AuthenticationSeeder.php

---
+++ AuthenticateService/database/seeds/AuthenticationSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AuthenticationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $MAX_RANDOM = 2;
+        $provider_id = [1234567, 89101113];
+        $provider_name = ['facebook', 'facebook'];
+        $role = ['itim_applicant','itim_passing'];
+        $wip_id = null;
+        for($i = 0 ; $i < $MAX_RANDOM ; $i++){
+            DB::table('authentication')->insert([
+                'provider_id' => $provider_id[$i],
+                'provider_name' => $provider_name[$i],
+                'role' => $role[$i],
+                'wip_id' => $wip_id
+            ]);
+        }
+    }
+}
