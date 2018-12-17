afb5455c562fbb0153cbe9fc823765241b1f73e4
Mild-TN <mild20091931@gmail.com>
2018-12-18 00:59:58 +0700

    [UPDATE] change route

--- AuthenticateService/routes/api.php
+++ AuthenticateService/routes/api.php
@@ -13,6 +13,21 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::post('login', 'AuthController@login');
+// Route::middleware('jwt.auth')->get('users', function () {
+//     return auth('api')->user();
+// });
+
+Route::group([
+  'middleware' => 'api',
+  'prefix' => 'auth'
+], function ($router) {
+  Route::post('login', 'AuthController@login');
+  Route::post('logout', 'AuthController@logout');
+  Route::post('refresh', 'AuthController@refresh');
+  Route::post('me', 'AuthController@me');
 });
