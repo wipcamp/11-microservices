218c5feffc5fa1cd6ff2745600754b6f82092d85
Mild-TN <mild20091931@gmail.com>
2018-12-18 01:01:44 +0700

    [UPDATE] change providers and api defult

--- AuthenticateService/config/auth.php
+++ AuthenticateService/config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api',
         'passwords' => 'users',
     ],
 
@@ -42,7 +42,7 @@ return [
         ],
 
         'api' => [
-            'driver' => 'token',
+            'driver' => 'jwt',
             'provider' => 'users',
         ],
     ],
@@ -67,12 +67,12 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => App\Models\Authentication::class,
+                // 'table' => 'credential',
         ],
 
         // 'users' => [
         //     'driver' => 'database',
-        //     'table' => 'users',
         // ],
     ],
 
