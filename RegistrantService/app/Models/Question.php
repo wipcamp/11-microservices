17c07c3e03a9032cac51f33e6cce6c934d9af94d
JaysZila <methaporn.jz@mail.kmutt.ac.th>
2018-12-17 17:56:59 +0700

    [Update] hidden weight attribute

--- RegistrantService/app/Models/Question.php
+++ RegistrantService/app/Models/Question.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     protected $table = 'questions';
+    protected $hidden = ['intelligent_weight','communication_weight','creative_weight'];
 }
