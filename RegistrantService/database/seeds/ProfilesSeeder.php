17edb3bf0c6a85d4e4845e63f93a02e3b1633293
Mild-TN <mild20091931@gmail.com>
2018-12-18 13:21:55 +0700

    [ADD] ProfilesSeeder

---
+++ RegistrantService/database/seeds/ProfilesSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProfilesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        for ($i=0; $i < 2; $i++) { 
+            DB::table('profiles')->insert([
+                'fistname_th' =>'ทินภัทร',
+                'lastname_th' => 'มงคลธนโชค',
+                'fistname_en' => null,
+                'lastname_en' => null,
+                'telno' => null,
+                'nickname' => 'fame',
+                'gender' => 'male',
+                'school_id' => null,
+                'school_name' => null,
+                'school_level' => null,
+                'school_major'=> null,
+                'gpax' => null,
+                'religion' => null,
+                'allergic_food' => null,
+                'allergic_drug' => null,
+                'cangenital_disease' => null,
+                'email' => null,
+                'dob' => null,
+                'citizen_no' => 1041672158743,
+                'guardian_relative' => null,
+                'guardian_telno' => null,
+                'medical_approved' => null,
+                ]);
+            }
+    }
+}
