a4c62a37adc275640b34f7e4dc8d52ff113bd9c4
Mild-TN <mild20091931@gmail.com>
2018-12-18 18:45:24 +0700

    [ADD] ProfileRepositories

---
+++ RegistrantService/app/Repositories/ProfileRepository.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Repositories;
+
+use DB;
+use Illuminate\Http\Request;
+use App\Models\Profile;
+
+class ProfileRepository implements ProfileRepositoryInterface
+{
+  public function getProfile(Type $var = null)
+  {
+    
+  }
+}
