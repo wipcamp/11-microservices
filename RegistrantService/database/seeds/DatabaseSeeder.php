4c50a1efb7016cf09ca6b6fb330a68d9dffd0e64
JaysZila <methaporn.jz@mail.kmutt.ac.th>
2018-12-18 14:45:14 +0700

    [ADD] add AnswerSeeder and Answer migration

--- RegistrantService/database/seeds/DatabaseSeeder.php
+++ RegistrantService/database/seeds/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             QuestionsSeeder::class,
-            ProfilesSeeder::class
+            ProfilesSeeder::class,
+            AnswersSeeder::class
         ]);
 
     }
