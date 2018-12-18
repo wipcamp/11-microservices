3d4867ccf58e651eb1f3af65981ff4f36ddac4c5
Mild-TN <mild20091931@gmail.com>
2018-12-18 23:44:06 +0700

    [UPDATE] add class SchoolSeeder

--- RegistrantService/database/seeds/DatabaseSeeder.php
+++ RegistrantService/database/seeds/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             QuestionsSeeder::class,
             ProfilesSeeder::class,
-            AnswersSeeder::class
+            AnswersSeeder::class,
+            SchoolSeeder::class,
         ]);
 
     }
