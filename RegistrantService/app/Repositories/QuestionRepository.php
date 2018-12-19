7e5d5912cdc08563ff7c921cbe22fe7acfab7d01
JaysZila <methaporn.jz@mail.kmutt.ac.th>
2018-12-20 02:27:58 +0700

    [UPDATE] edit find questions response

--- RegistrantService/app/Repositories/QuestionRepository.php
+++ RegistrantService/app/Repositories/QuestionRepository.php
@@ -4,17 +4,18 @@ namespace App\Repositories;
 use App\Models\Question;
 use App\Repositories\QuestionRepositoryInterface;
 use App\Repositories;
+use DB;
 
 
 class QuestionRepository implements QuestionRepositoryInterface{
 
     public function findAllQuesions(){
-        $questions = Question::get();
-        return $questions;
+        return Question::get();
     }
     public function findQuestionById($question_id){
         $question = Question::where('id',$question_id)->first();
        return $question;
     }
 
-}
+    }
+
