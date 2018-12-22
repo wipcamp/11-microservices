053fad6c6fb8cbe1a90d21127857252f6e47efa9
JewlyTwin <l2jew555@hotmail.com>
2018-12-22 15:34:20 +0700

    [ADD] Migrate Sponsor

---
+++ SponsorService/database/migrations/2018_12_19_103534_create_sponsor_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSponsorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sponsor', function (Blueprint $table) {
+            $table->increments('sponsor_id'); 
+            $table->string('sponsor_name');
+            $table->integer('sponsor_order');
+            $table->string('sponsor_path');
+            $table->timestamp('updated_at')->useCurrent();
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sponsor');
+    }
+}
