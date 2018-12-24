cdd21b5d22f2e84d37dc9fd721b77ecf3611ff0f
JaysZila <methaporn.jz@mail.kmutt.ac.th>
2018-12-24 13:57:28 +0700

    [UPDATE] use Mild code and edit conflict

--- RegistrantService/app/Http/Controllers/ProfileController.php
+++ RegistrantService/app/Http/Controllers/ProfileController.php
@@ -42,29 +42,30 @@ class ProfileController extends Controller
         $validator = Validator::make($req->all(), [
             'wip_id' => 'required',
             'prefix_name' => 'required|max:6',
-            'fistname_th' => 'required',
-            'lastname_th' => 'required',
-            'fistname_en'=>'required',
-            'lastname_en'=>'required',
-            'telno'=>'required',
-            'nickname'=>'required',
-            'gender'=>'required',
-            'school_id'=>'required',
-            'school_name'=>'required',
-            'school_level'=>'required',
+            'fistname_th' => 'required|string',
+            'lastname_th' => 'required|string',
+            'fistname_en'=>'required|string',
+            'lastname_en'=>'required|string',
+            'telno'=>'required|string|max:10',
+            'nickname'=>'required|string|max:10',
+            'gender'=>'required|string|max:6',
+            'school_id'=>'required|string|max:3',
+            'school_name'=>'required|string',
+            'school_level'=>'required|string',
             'gpax'=>'required',
-            'religion'=>'required',
+            'religion'=>'required|string|max:20',
             'allergic_food'=>'required',
-            'email'=>'required',
-            'dob'=>'required',
-            'citizen_no'=>'required',
+            'email'=>'required|email',
+            'dob'=>'required|date',
+            'citizen_no'=>'required|max:13',
             'guardian_relative'=>'required',
             'guardian_telno'=>'required',
+            'guardian_telno'=>'required:max10',
         ]);
 
         if ($validator->fails()) {
             return response()->json([
-                'error' => 'Invalid test.'
+                'error' => 'Invalid feild !'
             ]);
         }
         $profile = $req->all();
