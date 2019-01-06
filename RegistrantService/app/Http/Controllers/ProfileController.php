2c4c1aeccf958338f3988648e1a2512084d92c21
Mild-TN <mild20091931@gmail.com>
2019-01-06 16:03:33 +0700

    [UPDATE] change type citizen_no

--- RegistrantService/app/Http/Controllers/ProfileController.php
+++ RegistrantService/app/Http/Controllers/ProfileController.php
@@ -57,7 +57,7 @@ class ProfileController extends Controller
             'allergic_food'=>'required',
             'email'=>'required|email',
             'dob'=>'required|date',
-            'citizen_no'=>'required|max:13',
+            'citizen_no'=>'required|string|max:13',
             'guardian_relative'=>'required',
             'guardian_telno'=>'required',
             'guardian_telno'=>'required:max10',
--- RegistrantService/database/migrations/2018_12_18_034223_profiles.php
+++ RegistrantService/database/migrations/2018_12_18_034223_profiles.php
@@ -17,7 +17,7 @@ class Profiles extends Migration
             $table->collation = 'utf8_general_ci';
             $table->charset = 'utf8';
             $table->increments('wip_id');
-            $table->string('prefix_name')->nullable();
+            $table->string('prefix_name',20)->nullable();
             $table->string('fistname_th', 100)->nullable();
             $table->string('lastname_th', 100)->nullable();
             $table->string('fistname_en', 100)->nullable();
@@ -27,7 +27,7 @@ class Profiles extends Migration
             $table->enum('gender', ['male', 'female'])->nullable();
             $table->string('school_id', 3)->nullable();
             $table->string('school_name', 100)->nullable();
-            $table->string('school_level', 2)->nullable();
+            $table->string('school_level',15)->nullable();
             $table->string('school_major', 100)->nullable();
             $table->string('gpax', 4)->nullable();
             $table->string('religion', 100)->nullable();
@@ -36,15 +36,15 @@ class Profiles extends Migration
             $table->string('cangenital_disease', 100)->nullable();
             $table->string('email', 100)->nullable();
             $table->date('dob')->nullable();
-            $table->bigInteger('citizen_no',13)->nullable();
+            $table->string('citizen_no',13)->nullable();
             $table->string('guardian_relative', 20)->nullable();
             $table->string('guardian_telno', 10)->nullable();
-            $table->boolean('medical_approved')->nullable();
-            $table->boolean('confirm_register')->nullable();
+            $table->boolean('medical_approved',false)->nullable();
+            $table->boolean('confirm_register',false)->nullable();
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
-        DB::update("ALTER TABLE profiles AUTO_INCREMENT = 110001;");
+        DB::statement("ALTER TABLE profiles AUTO_INCREMENT = 110001;");
     }
 
     /**
