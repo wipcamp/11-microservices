ad43d1586481852b780036ad4f7014210f643020
Mild-TN <mild20091931@gmail.com>
2019-01-12 18:16:46 +0700

    [ADD] สร้าง RolePermissionRepository.php

---
+++ AuthenticateService/app/Repositories/RolePermissionRepository.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Repositories;
+
+use DB;
+use Illuminate\Http\Request;
+use App\Models\RolePermission;
+use App\Repositories\RolePermissionRepositoryInterface;
+
+class RolePermissionRepository implements RolePermissionRepositoryInterface
+{
+  
+}
