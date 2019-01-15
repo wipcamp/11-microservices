73e61578da4da0ac551c7a99129c03b945cbfb20
Mild-TN <mild20091931@gmail.com>
2019-01-15 19:42:19 +0700

    [UPDATE] andle CheckAuth

--- AuthenticateService/app/Http/Middleware/CheckAuth.php
+++ AuthenticateService/app/Http/Middleware/CheckAuth.php
@@ -17,9 +17,46 @@ class CheckAuth
     public function handle($request, Closure $next)
     {
         $jwt_secret = env('JWT_SECRET');
-        $token = $request->header('authorization');
-        $jwt = substr($token, 7);
-        $decoded = JWT::decode($jwt, $jwt_secret, array('HS256'));
-        return $next($request);
+        $token = $request->header('Authorization');
+        $tokenBearer = substr($token,0,7);
+        $jwt = substr($token,7);
+        $check = $this->checkToken($tokenBearer,$token);
+        if($check){
+            try {
+                $decoded = JWT::decode($jwt, $jwt_secret, array('HS256'));
+                $wipId = $decoded->sub;
+                $request['wip_id'] = $wipId;
+                return $next($request);
+            }
+             catch (ExpiredException $exp) {
+                return response()->json([
+                    'error' => 'Expire token.'],401
+                );
+            } 
+            catch (\Exception $ex)  {
+                return response()->json(['error' => 'Invalid token.'],401);
+            }
+        }else{
+            return response()->json([
+                'error' => 'Invalid User.'
+            ],401);
+        }
+    }
+
+    public function checkToken($tokenBearer,$token)
+    {
+        if(!is_null($token)){
+            if(!is_null($tokenBearer)){
+                if($tokenBearer =='Bearer '){
+                    return true;
+                }else {
+                    return false;
+                }
+            }
+        }else {
+            return response()->json([
+                'error' => 'Token is null.'
+            ],401);
+        }
     }
 }
