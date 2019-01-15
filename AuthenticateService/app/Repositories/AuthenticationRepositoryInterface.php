0908c8b32199ffe3a18a8c9e7c36f4fcc815ad11
Mild-TN <mild20091931@gmail.com>
2019-01-15 19:41:36 +0700

    [UPDATE] return permission and role

--- AuthenticateService/app/Repositories/AuthenticationRepositoryInterface.php
+++ AuthenticateService/app/Repositories/AuthenticationRepositoryInterface.php
@@ -5,4 +5,6 @@ interface AuthenticationRepositoryInterface{
   public function getByProviderId($provider_id);
   public function createUser($data);
   public function updateByProviderId($provider_id, $wip_id);
+  public function getRoleByWipId($wip_id);
+  public function getPermissionByWipId($wip_id);
 }
