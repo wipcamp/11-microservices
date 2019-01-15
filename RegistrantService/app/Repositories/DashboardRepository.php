c1526216c527e52f6a329005890fbb1d026f7264
JaysZila <methaporn.jz@mail.kmutt.ac.th>
2019-01-15 20:46:00 +0700

    สร้างไฟล์ DashboardRepository

---
+++ RegistrantService/app/Repositories/DashboardRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\DashboardRepositoryInterface;
+use App\Models\Registrant;
+
+class DashboardRepository implements DashboardRepositoryInterface{
+
+}
