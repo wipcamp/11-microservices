08491ed37df7c1aba2a92b46a221b741ddf6fab0
pepea <pepea.23@live.com>
2019-02-16 12:42:07 +0700

    [UPDATE] Header check

--- gameservice/routes/api.php
+++ gameservice/routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token,Authorization');
+header('Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS');
 
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
