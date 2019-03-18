afc1d69fc2144f79b26025ef10f286f26e083a24
pepea <pepea.23@live.com>
2019-03-18 19:16:14 +0700

    [UPDATE] check selsect *

--- RegistrantService/app/Repositories/AnswerRepository.php
+++ RegistrantService/app/Repositories/AnswerRepository.php
@@ -26,7 +26,7 @@ class AnswerRepository implements AnswerRepositoryInterface
     public function getAnswersByQuestionsId($question_id,$wip_id)
     {   
         
-        return Answer::select('ans_id','ans_content','wip_id')->leftJoin('answer_evaluations','answer_evaluations.question_id','answers.question_id')->where('answers.question_id',$question_id)->get();
+        return Answer::select('*')->leftJoin('answer_evaluations','answer_evaluations.question_id','answers.question_id')->where('answers.question_id',$question_id)->get();
     }
 
     public function getAnswersByQuestionbywipId($question_id,$wip_id)
