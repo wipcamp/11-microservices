4736e1aec38432d3ceb2deeca6fb873f9c4a3695
papoppluem <papop.ru@mail.kmutt.ac.th>
2019-03-22 16:20:50 +0700

    [UPDATE] check if ไม่ให้ AnswerEvaluation create เกิน 3 rows

--- RegistrantService/app/Repositories/AnswerEvaluationRepository.php
+++ RegistrantService/app/Repositories/AnswerEvaluationRepository.php
@@ -9,12 +9,18 @@ class  AnswerEvaluationRepository implements AnswerEvaluationRepositoryInterface
 {
   public function answerEvaluations($evaluation)
   {
-      return AnswerEvaluation::create([
+      $checkanswer_id = $evaluation['answer_id'];
+      $check = AnswerEvaluation::where('answer_id',$checkanswer_id)->count();
+      if($check < 3){
+        return AnswerEvaluation::create([
         'answer_id' => $evaluation['answer_id'],
         'checker_wip_id' => $evaluation['wip_id'],
         'question_id' => $evaluation['question_id'],
         'score' => $evaluation['score'],
         'score_category' => $evaluation['score_category']
       ]);
+      } else{
+        return 'hello';
+      }
   }
 }
