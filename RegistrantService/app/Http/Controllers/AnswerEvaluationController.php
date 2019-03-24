71edb0ffd5a2815977c336f4d9f53df9199e0818
pepea <pepea.23@live.com>
2019-03-24 18:59:55 +0700

    [UPDATE] check permission checker

--- RegistrantService/app/Http/Controllers/AnswerEvaluationController.php
+++ RegistrantService/app/Http/Controllers/AnswerEvaluationController.php
@@ -17,7 +17,19 @@ class AnswerEvaluationController extends Controller
 
   public function answerEvaluations(Request $req)
   {
-      
+
+    $data = $req->all();
+    $wipId = $req->all()['wip_id'];
+    $token = $req->header('Authorization');
+    $URL = env('AUTH_URL') . '/permissions';
+    $headers = ['Authorization' => $token];
+    $client = new \GuzzleHttp\Client(['base_uri' => $URL,'headers' => $headers]);
+    $response = $client->request('GET',$URL,['query' => ['wip_id' => $wipId]]);
+    $response = json_decode($response->getBody(),true);
+    $arr_res = Arr::dot($response['permission']);
+    $arr_res = Arr::flatten($arr_res);
+    
+    if(in_array(4,$arr_res)||in_array(10,$arr_res)){
       $evaluations = $req->all();
       $wip_id = $evaluations['wip_id'];
       $evaluations = array_except($evaluations, ['wip_id']);
@@ -25,9 +37,14 @@ class AnswerEvaluationController extends Controller
 
       for($i=0;$i<count($evaluations);$i++){
         $this->answerEvaluations->answerEvaluations($evaluations[$i]);
-
       }
-      
       return response()->json(["messege" => 'post Evaluations success ! '],201);
+
+    }else{
+      return response()->json(['error' => "role or permission invalid !!"],405);
+    }
+
+      
+      
   }
 }
