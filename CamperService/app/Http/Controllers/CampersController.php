4b95de620208d2a1dd58b849dab1cf7ac906e368
pepea <pepea.23@live.com>
2019-03-27 16:06:40 +0700

    [ADD] method + provider to create minio

--- CamperService/app/Http/Controllers/CampersController.php
+++ CamperService/app/Http/Controllers/CampersController.php
@@ -6,8 +6,11 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\CampersController;
 use App\Repositories\CampersRepository;
 use App\Repositories\CampersRepositoryInterface;
+use Aws\StorageGateway\StorageGatewayClient;
 
 use Illuminate\Http\Request;
+
+use Storage;
 
 class CampersController extends Controller
 {
@@ -22,6 +25,13 @@ class CampersController extends Controller
      
    
     return response()->json(['test' => $camps]);
+  }
+  public function uploadFile(Request $req)
+  {
+    $created = Storage::disk('minio')->put('/test.txt','Hello World!');
+    $url = Storage::disk('minio')->url('/test.txt');
+
+    return  response()->json($url, 200);
   }
 }
 
