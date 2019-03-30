328912f1b1896e2430d680f52c05881a27f9d873
Mild-TN <mild20091931@gmail.com>
2019-03-30 23:30:20 +0700

    [UPDATE] แก้ set time path img

--- CamperService/app/Http/Controllers/CampersController.php
+++ CamperService/app/Http/Controllers/CampersController.php
@@ -35,8 +35,7 @@ class CampersController extends Controller
     $filename = ($wip_id.'_'.$path);
     $destinationPath = 'WIPID'.$wip_id.'/'.$filename;
     $created = Storage::disk('minio')->put($destinationPath,file_get_contents($file->getRealPath()));
-    // $url = Storage::disk('minio')->url($destinationPath);
-    $url = Storage::cloud()->temporaryUrl($destinationPath, \Carbon\Carbon::now()->addMinutes(1));
+    $url = Storage::cloud()->temporaryUrl($destinationPath, \Carbon\Carbon::now()->addMinutes(10));
     return  response()->json($url, 200);
   }
   public function getFile()
