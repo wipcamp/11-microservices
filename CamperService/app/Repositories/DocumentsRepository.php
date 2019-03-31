c985bf6b5cc3d6d00590c0e3b09b10099c3291cc
Mild-TN <mild20091931@gmail.com>
2019-03-31 23:18:53 +0700

    [UPDATE] check data

--- CamperService/app/Repositories/DocumentsRepository.php
+++ CamperService/app/Repositories/DocumentsRepository.php
@@ -5,9 +5,31 @@ use App\Repositories\DocumentsRepositoryInterface;
 
 class DocumentsRepository implements DocumentsRepositoryInterface
 {
-  public function ctreateDocBywipId($wipId,$path)
+  public function checkDocId($wipId)
   {
-    dd($wipId,$path);
-    $createDoc = Camper::insert('wip_id','doc_id');
+    $docId = 'doc_id_'.$wipId;
+    $docId = Documents::select('doc_id')->where('doc_id',$docId)->get();
+    return $docId->isEmpty();
+  }
+
+  public function ctreateDocBywipId($wipId,$path,$type)
+  {
+    $status = 'unsuccess'; 
+    $createDoc = Documents::create([
+      'doc_id' => 'doc_id_'.$wipId,
+       $type => $path,
+      'status' => $status,
+      'reason' => null,
+      'pick_location' => null,
+      'size' => null
+    ]);
+    return $createDoc;
+  }
+
+  public function updateDoc($wipId,$path,$type)
+  {
+    $docId = 'doc_id_'.$wipId;
+    $docId = Documents::where('doc_id',$docId)->update(array($type => $path));
+    return $docId;
   }
 }
