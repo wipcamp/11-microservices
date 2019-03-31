064bb3867764115978efbf3ef2adc0367bcbbde5
Mild-TN <mild20091931@gmail.com>
2019-03-31 23:18:06 +0700

    [UPDATE] add method in DocumentsRepositoryInterface.php

--- CamperService/app/Repositories/DocumentsRepositoryInterface.php
+++ CamperService/app/Repositories/DocumentsRepositoryInterface.php
@@ -1,5 +1,7 @@
 <?php
 namespace App\Repositories;
 interface DocumentsRepositoryInterface {
-    public function ctreateDocBywipId($wipId,$path);
+    public function ctreateDocBywipId($wipId,$path,$type);
+    public function updateDoc($wipId,$path,$type);
+    public function checkDocId($wipId);
 }
