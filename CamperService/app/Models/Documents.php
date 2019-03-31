340bd479e4926ce33d9b7c6fa2d621a363b851dd
Mild-TN <mild20091931@gmail.com>
2019-03-31 23:20:47 +0700

    [UPDATE] add fillable

--- CamperService/app/Models/Documents.php
+++ CamperService/app/Models/Documents.php
@@ -1,10 +1,25 @@
 <?php
-
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Documents extends Model
 {
     protected $table = 'documents';
+
+    protected $primaryKey = 'doc_id';
+
+    protected $fillable = ['doc_id',
+                            'pick_location',
+                            'size',
+                            'transcript',
+                            'confrim',
+                            'receipt',
+                            'reason',
+                            
+                        ];
+    protected $hidden =['created_at','updated_at'];
+
+    public $timestamps = false;
+    public $timestamp = false;
 }
