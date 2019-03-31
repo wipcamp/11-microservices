e294bc81cc11df136058c54577cdad3c4cd10289
Mild-TN <mild20091931@gmail.com>
2019-03-31 23:57:38 +0700

    [UPDATE] add getDocumentByWipId() in RepoInterface

--- CamperService/app/Repositories/DocumentsRepositoryInterface.php
+++ CamperService/app/Repositories/DocumentsRepositoryInterface.php
@@ -4,4 +4,5 @@ interface DocumentsRepositoryInterface {
     public function ctreateDocBywipId($wipId,$path,$type);
     public function updateDoc($wipId,$path,$type);
     public function checkDocId($wipId);
+    public function getDocumentByWipId($wipId);
 }
