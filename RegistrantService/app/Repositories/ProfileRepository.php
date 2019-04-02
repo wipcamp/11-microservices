d2acaf5e299ae6d4166184706552470af5832959
pepea <pepea.23@live.com>
2019-04-02 23:33:42 +0700

    [UPDATE] merge doc+profile

--- RegistrantService/app/Repositories/ProfileRepository.php
+++ RegistrantService/app/Repositories/ProfileRepository.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Repositories;
+use Illuminate\Support\Arr;
 
 use App\Models\Profile;
 class ProfileRepository implements ProfileRepositoryInterface
@@ -12,12 +13,22 @@ class ProfileRepository implements ProfileRepositoryInterface
     public function getProfiles($data)
     {
         $profiles = array();
+        $doc_id = array_pluck($data,'doc_id');
         for ($i=0; $i != count($data); $i++) { 
-        $wip_id = substr($data[$i],7);
+        $wip_id = substr($doc_id[$i],7);
         $profile = Profile::where('wip_id',$wip_id)->get();
+        $profile[0]->doc_id=$data[$i]['doc_id'];
+        $profile[0]->status=$data[$i]['status'];
+        $profile[0]->reason=$data[$i]['reason'];
+        $profile[0]->transcript=$data[$i]['transcript'];
+        $profile[0]->confrim=$data[$i]['confrim'];
+        $profile[0]->receipt=$data[$i]['receipt'];
+        $profile[0]->size=$data[$i]['size'];
+        $profile[0]->pick_location=$data[$i]['pick_location'];
         array_push($profiles,$profile);
         }
-
+    
+      
         return $profiles;
     }
 
